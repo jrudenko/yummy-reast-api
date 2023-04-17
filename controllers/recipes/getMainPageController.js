c2a9c900cf4948257c91b948715330050f0b9713
DimaKrupenko <dimakrupenko77@gmail.com>
2023-04-17 19:25:32 +0300

    upd

--- controllers/recipes/getMainPageController.js
+++ controllers/recipes/getMainPageController.js
@@ -6,11 +6,21 @@ const { Recipes } = require('../../db/recipesModel');
 const { catchAsyncWrapper } = require('../../utils');
 
 const getMainPageController = catchAsyncWrapper(async (req, res) => {
-  const result = await Recipes.find({}).limit(4);
+  //   const result = await Recipes.find({}).limit(4);
+  const categories = await Recipes.distinct('category');
+  const selectedCategories = categories.slice(0, 4);
+  const tasks = selectedCategories.map(category => async () => {
+    const result = await Recipes.find({ category }).limit(4);
+    return result;
+  });
+  const result = await Promise.all(tasks.map(task => task()));
+
+  const flattenedResult = result.reduce((acc, curr) => acc.concat(curr), []);
+
   res.json({
     status: 'success',
     code: 200,
-    result,
+    flattenedResult,
   });
 });
 
