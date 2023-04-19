574bdb3fe2c0b9e7f974941ab0e047d77f501f60
DimaKrupenko <dimakrupenko77@gmail.com>
2023-04-19 18:04:05 +0300

    upd

--- controllers/recipes/index.js
+++ controllers/recipes/index.js
@@ -1,11 +1,11 @@
 const getCategoryListController = require('./getCategoryListController');
 const getMainPageController = require('./getMainPageController');
 const searchByCategoryController = require('./searchByCategoryController');
-// const searchByIdController = require('./searchByIdController');
+const searchByIdController = require('./searchByIdController');
 
 module.exports = {
   getCategoryListController,
   getMainPageController,
   searchByCategoryController,
-  //   searchByIdController,
+  searchByIdController,
 };
---
+++ controllers/recipes/searchByIdController.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const { recipes } = require('../../services');
+
+const { catchAsyncWrapper } = require('../../utils');
+
+const searchByIdController = catchAsyncWrapper(async (req, res) => {
+  const { recipesId } = req.params;
+  const result = await recipes.searchById(recipesId);
+
+  if (result.length === 0) {
+    return res.status(204).json();
+  }
+  res.json({
+    status: 'success',
+    code: 200,
+    result,
+  });
+});
+
+module.exports = searchByIdController;
