fca1d9ab5af644b5516180231e7b8c0ebed5c99e
DimaKrupenko <dimakrupenko77@gmail.com>
2023-04-20 14:54:34 +0300

    upd

--- middlewares/index.js
+++ middlewares/index.js
@@ -5,11 +5,13 @@ const {
 } = require('./usersValidate');
 const { auth } = require('./auth');
 const { queryStringValidate } = require('./queryStringValidate');
+const { upload } = require('./upload');
 
 module.exports = {
   createUserValidate,
   loginUserValidate,
   auth,
   subscribeUserValidate,
+  upload,
   queryStringValidate,
 };
