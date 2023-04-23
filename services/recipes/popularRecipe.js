64dac88a50650d5e6705ad95bae34e00d5c7be25
DimaKrupenko <dimakrupenko77@gmail.com>
2023-04-23 20:36:04 +0300

    upd: fixed popular recipes

--- services/recipes/popularRecipe.js
+++ services/recipes/popularRecipe.js
@@ -11,6 +11,34 @@ const popularRecipe = async () => {
       },
     },
     { $sort: { count: -1 } },
+    {
+      $lookup: {
+        from: 'recipes',
+        localField: '_id',
+        foreignField: '_id',
+        as: 'recipe',
+      },
+    },
+    { $unwind: '$recipe' },
+    {
+      $project: {
+        _id: '$recipe._id',
+        title: '$recipe.title',
+        category: '$recipe.category',
+        area: '$recipe.area',
+        instructions: '$recipe.instructions',
+        description: '$recipe.description',
+        thumb: '$recipe.thumb',
+        preview: '$recipe.preview',
+        time: '$recipe.time',
+        youtube: '$recipe.youtube',
+        tags: '$recipe.tags',
+        createdAt: '$recipe.createdAt',
+        updatedAt: '$recipe.updatedAt',
+        ingredients: '$recipe.ingredients',
+        count: 1,
+      },
+    },
   ]);
 
   return popularRecipes;
