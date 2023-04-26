7984101e3faef60198d0e8f7a55778e990bd6a20
Yuriy Gomenyuk <yourgo@gmail.com>
2023-04-26 10:37:29 +0300

    temp

--- middlewares/joiTemplates.js
+++ middlewares/joiTemplates.js
@@ -1,7 +1,12 @@
 const Joi = require('joi');
 
 const name = Joi.string().pattern(/^[a-zA-Zа-яА-Я0-9іІїЇєЄґҐ']{1,16}$/);
-const email = Joi.string().email();
+const email = Joi.string()
+  .email()
+  .regex(/^[а-яА-Я]$/);
+/* const email = Joi.string()
+  // .pattern(/^[a-zA-Z0-9']{1,16}$/)
+  .email(); */
 const password = Joi.string().pattern(/^[a-zA-Z0-9.;,/.;'`)(*&^%$#@!~]{6,16}$/);
 const verificationToken = Joi.string().guid({ version: 'uuidv4' });
 const queryString = Joi.string().min(2).max(20);
