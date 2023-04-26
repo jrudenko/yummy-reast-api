506f4673f8549753fcd6a41c6580dc7e29a74585
DimaKrupenko <dimakrupenko77@gmail.com>
2023-04-26 15:39:41 +0300

    upd: finished pagination

--- services/ownRecipes/getOwnRecipes.js
+++ services/ownRecipes/getOwnRecipes.js
@@ -1,9 +1,19 @@
 require('dotenv').config();
 const { Recipes } = require('../../db/recipesModel');
 
-const getOwnRecipes = async userId => {
-  const recipes = await Recipes.find({ owner: userId }).lean();
-  return recipes;
+const getOwnRecipes = async (userId, pageNumber, pageSize) => {
+  const skip = (pageNumber - 1) * pageSize;
+  const count = await Recipes.countDocuments({ owner: userId });
+  const recipes = await Recipes.find({ owner: userId })
+    .skip(skip)
+    .limit(pageSize)
+    .lean();
+  return {
+    totalResults: count,
+    pageSize,
+    currentPage: pageNumber,
+    results: recipes,
+  };
 };
 
 module.exports = getOwnRecipes;
