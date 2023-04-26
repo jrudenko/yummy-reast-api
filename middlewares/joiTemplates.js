fb4ca24da4444d3603d29c828bb35c657f79f90e
Yuriy Gomenyuk <yourgo@gmail.com>
2023-04-26 11:34:55 +0300

    fixed email & password joi validation

--- middlewares/joiTemplates.js
+++ middlewares/joiTemplates.js
@@ -3,11 +3,13 @@ const Joi = require('joi');
 const name = Joi.string().pattern(/^[a-zA-Zа-яА-Я0-9іІїЇєЄґҐ']{1,16}$/);
 const email = Joi.string()
   .email()
-  .regex(/^[а-яА-Я]$/);
-/* const email = Joi.string()
-  // .pattern(/^[a-zA-Z0-9']{1,16}$/)
-  .email(); */
-const password = Joi.string().pattern(/^[a-zA-Z0-9.;,/.;'`)(*&^%$#@!~]{6,16}$/);
+  .required()
+  .pattern(/^[^\u0400-\u04FF]*$/);
+
+const password = Joi.string().pattern(
+  /^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9.;,/.;'`)(*&^%$#@!~]{6,16}$/
+);
+
 const verificationToken = Joi.string().guid({ version: 'uuidv4' });
 const queryString = Joi.string().min(2).max(20);
 const typeString = Joi.string().valid('title', 'ingredients');
