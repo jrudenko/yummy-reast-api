4654d4ccd19f8ba7cda4d5ff45f20ec43fdb6494
DimaKrupenko <dimakrupenko77@gmail.com>
2023-04-28 23:18:35 +0300

    upd

--- services/ownRecipes/index.js
+++ services/ownRecipes/index.js
@@ -1,5 +1,11 @@
 const removeOwnRecipes = require('./removeOwnRecipes');
 const getOwnRecipes = require('./getOwnRecipes');
 const addOwnRecipe = require('./addOwnRecipe');
+const getOwnRecipesById = require('./getOwnRecipesById');
 
-module.exports = { removeOwnRecipes, getOwnRecipes, addOwnRecipe };
+module.exports = {
+  removeOwnRecipes,
+  getOwnRecipes,
+  addOwnRecipe,
+  getOwnRecipesById,
+};
