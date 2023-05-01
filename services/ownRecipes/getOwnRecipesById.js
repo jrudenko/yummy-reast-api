42cc3946e5b2d3339761d578a5010fd21005bfde
Yuriy Gomenyuk <yourgo@gmail.com>
2023-05-01 13:07:49 +0300

    own recipe

--- services/ownRecipes/getOwnRecipesById.js
+++ services/ownRecipes/getOwnRecipesById.js
@@ -2,11 +2,18 @@ const { Recipes } = require('../../db/recipesModel');
 
 const getOwnRecipesById = async recipeId => {
   const recipes = await Recipes.findById(recipeId);
+  // console.log(
+  //   'CL: ~ file: getOwnRecipesById.js:5 ~ recipes:',
+  //   recipes.ingredients
+  // );
   //   return recipes;
-  const ingredients = recipes.ingredients[0];
-  const returnedRecipes = [recipes, ...ingredients];
 
-  return returnedRecipes;
+  const ingredients = JSON.parse(recipes.ingredients);
+
+  // console.log('~recipes.ingredients[0] getOwnRecipesById.js [9]:', recipes.ingredients[0]);
+  recipes.ingredients = ingredients;
+
+  return recipes;
 };
 
 module.exports = getOwnRecipesById;
